aeac3abea4ca147ca2e2d5907fef58d78a14dd2e
ctkremer <colin.kremer@yale.edu>
2018-07-31 09:28:04 -0400

    updated to-do list based on recent additions to package

--- TODO.R
+++ TODO.R
@@ -1,6 +1,8 @@
 # To-do list for package:
 
-
+# - allow for weighted regression based on reported standard error values
+# - combine nbcurve and nbcurve2 into a single function that depends on either copt or opt (see alternative parameterizations of distributions, e.g.)
+# - add sample size and logLik values to output from get.__curve.tpc functions, to support subsequent model comparison?
 
 # NOTES:
 
