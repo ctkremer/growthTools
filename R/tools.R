442021633feb8384ef2dd8c693ce7bbd6aed10ce
ctkremer <colin.kremer@yale.edu>
2018-10-04 09:22:13 -0400

    adding additional metrics to growth rate model fitting, to allow for down-stream quality control of fits, based on the number of observations present within the exponential phase portion of the regression, and the R2 of the regression within this same portion.

--- R/tools.R
+++ R/tools.R
@@ -308,6 +308,8 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
   class(modlist$gr)<-class(modlist$gr.lag)<-class(modlist$gr.sat)<-class(modlist$gr.lagsat)<-'try-error'
   gr<-gr.lag<-gr.sat<-gr.lagsat<-NA
   slope.gr<-slope.gr.lag<-slope.gr.sat<-slope.gr.lagsat<-NA
+  slope.n.gr<-slope.n.gr.lag<-slope.n.gr.sat<-slope.n.gr.lagsat<-NA
+  slope.r2.gr<-slope.r2.gr.lag<-slope.r2.gr.sat<-slope.r2.gr.lagsat<-NA
   se.gr<-se.gr.lag<-se.gr.sat<-se.gr.lagsat<-NA
   
   if(length(unique(x))==2){
@@ -321,31 +323,68 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
     if('linear' %in% methods){
       gr<-get.gr(x,y) 
       slope.gr<-coef(gr)[[2]]
+      slope.n.gr<-length(x)
+      slope.r2.gr<-NA
       se.gr<-sqrt(diag(vcov(gr)))['x']
       modlist$gr<-gr
     }
     if('lag' %in% methods){
       gr.lag<-try(get.gr.lag(x,y))  
-      slope.gr.lag<-ifelse(prod(class(gr.lag)!='try-error'),
-                         exp(coef(gr.lag)[3]),NA)
-      se.gr.lag<-ifelse(prod(class(gr.lag)!='try-error'),
-                        sqrt(diag(vcov(gr.lag)))['logb'],NA)
+      if(prod(class(gr.lag)!='try-error')){
+        b1.cutoff <- coef(gr.lag)[1]  # where does exponential phase begin?
+        pds.lag <- predict(gr.lag)[x>=b1.cutoff] # predicted values above this cutoff
+        obs.lag <- y[x>=b1.cutoff] # observed values above this cutoff
+        
+        slope.gr.lag <- exp(coef(gr.lag)[3])
+        slope.n.gr.lag <- length(x[x>=b1.cutoff])  # how many observations above cutoff
+        slope.r2.gr.lag <- 1-sum((pds.lag-obs.lag)^2)/sum((obs.lag-mean(obs.lag))^2)  # r2
+        se.gr.lag <- sqrt(diag(vcov(gr.lag)))['logb']
+      }else{
+        slope.gr.lag<-NA
+        slope.n.gr.lag<-NA
+        slope.r2.gr.lag<-NA
+        se.gr.lag<-NA
+      }
       modlist$gr.lag<-gr.lag
     }
     if('sat' %in% methods){
       gr.sat<-try(get.gr.sat(x,y))
-      slope.gr.sat<-ifelse(prod(class(gr.sat)!='try-error'),
-                         exp(coef(gr.sat)[3]),NA)
-      se.gr.sat<-ifelse(prod(class(gr.sat)!='try-error'),
-             sqrt(diag(vcov(gr.sat)))['logb'],NA)
+      if(prod(class(gr.sat)!='try-error')){
+        b2.cutoff <- coef(gr.sat)[1]  # where does exponential phase end?
+        pds.sat <- predict(gr.sat)[x<=b2.cutoff] # predicted values below this cutoff
+        obs.sat <- y[x<=b2.cutoff] # observed values below this cutoff
+        
+        slope.gr.sat <- exp(coef(gr.sat)[3])
+        slope.n.gr.sat <- length(x[x<=b2.cutoff])  # how many observations below cutoff
+        slope.r2.gr.sat <- 1-sum((pds.sat-obs.sat)^2)/sum((obs.sat-mean(obs.sat))^2)  # r2
+        se.gr.sat <- sqrt(diag(vcov(gr.sat)))['logb']
+      }else{
+        slope.gr.sat <- NA
+        slope.n.gr.sat <- NA
+        slope.r2.gr.sat <- NA
+        se.gr.sat <- NA
+      }
       modlist$gr.sat<-gr.sat
     }
     if('lagsat' %in% methods){
       gr.lagsat<-try(get.gr.lagsat(x,y))
-      slope.gr.lagsat<-ifelse(prod(class(gr.lagsat)!='try-error'),
-                            exp(coef(gr.lagsat)[4]),NA)
-      se.gr.lagsat<-ifelse(prod(class(gr.lagsat)!='try-error'),
-                        sqrt(diag(vcov(gr.lagsat)))['logb'],NA)
+      if(prod(class(gr.sat)!='try-error')){
+        b1.cutoff <- coef(gr.lagsat)[1]  # where does exponential phase begin?
+        b2.cutoff <- coef(gr.lagsat)[2]  # where does exponential phase end?
+        pds.lagsat <- predict(gr.lagsat)[x<=b2.cutoff & x >=b1.cutoff] # predictions btwn cutoffs
+        obs.lagsat <- y[x<=b2.cutoff & x >=b1.cutoff] # observed values between cutoffs
+        
+        
+        slope.gr.lagsat <- exp(coef(gr.lagsat)[4])
+        slope.n.gr.lagsat <- length(x[x<=b2.cutoff & x >=b1.cutoff])  # how many obs btwn cutoffs
+        slope.r2.gr.lagsat <- 1-sum((pds.lagsat-obs.lagsat)^2)/sum((obs.lagsat-mean(obs.lagsat))^2)  # r2
+        se.gr.lagsat <- sqrt(diag(vcov(gr.lagsat)))['logb']
+      }else{
+        slope.gr.lagsat <- NA
+        slope.n.gr.lagsat <- NA
+        slope.r2.gr.lagsat <- NA
+        se.gr.lagsat <- NA
+      }
       modlist$gr.lagsat<-gr.lagsat
     }
   
@@ -362,11 +401,15 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
     mod.list<-list(gr,gr.lag,gr.sat,gr.lagsat)[successful.fits]
     slope.ests<-c(slope.gr,slope.gr.lag,slope.gr.sat,slope.gr.lagsat)[successful.fits]
     se.ests<-unname(c(se.gr,se.gr.lag,se.gr.sat,se.gr.lagsat)[successful.fits])
+    slope.n.vals<-c(slope.n.gr,slope.n.gr.lag,slope.n.gr.sat,slope.n.gr.lagsat)[successful.fits]
+    slope.r2.vals<-c(slope.r2.gr,slope.r2.gr.lag,slope.r2.gr.sat,slope.r2.gr.lagsat)[successful.fits]
       
     # compare successful models
     aictab<-AICtab(mod.list,mnames = mod.names)
     best.mod.id<-which(mod.names==attr(aictab,"row.names")[1])
-  
+    
+    # impose QC based on slope.n and slope.r2 here? or outside of function...
+      
     # format output:
     result<-list(best.slope=slope.ests[[best.mod.id]],
                  best.se=se.ests[[best.mod.id]],
@@ -375,6 +418,8 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
                  best.model.contents=list(mod.list[[best.mod.id]]),
                  slopes=slope.ests,
                  ses=se.ests,
+                 slope.ns=slope.n.vals,
+                 slope.rs=slope.r2.vals,
                  models=list(gr=gr,gr.lag=gr.lag,gr.sat=gr.sat,gr.lagsat=gr.lagsat))
     #print(result)
   
@@ -401,6 +446,8 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
                  best.model.contents=list(NA),
                  slopes=NA,
                  ses=NA,
+                 slope.ns=NA,
+                 slope.rs=NA,
                  models=list(gr=NA,gr.lag=NA,gr.sat=NA,gr.lagsat=NA))
   }
   
