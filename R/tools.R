6cf48f07f5946fb99ae3f39a238960f249e7313c
ctkremer <colin.kremer@yale.edu>
2018-10-16 11:58:52 -0400

    Added new functions

--- R/tools.R
+++ R/tools.R
@@ -65,6 +65,14 @@ sat<-function(x,a,b,B2,s=1E-10){
   a + (1/2)*b*(B2) + sqfunc(-x,b,s) - sqfunc(B2-x,b,s)
 }
 
+#' @describeIn lagsat Floored decreasing linear function
+#' @export
+flr<-function(x,a,b,B2,s=1E-10){
+  b <- -1*b
+  a - (1/2)*(b)*(B2) - sqfunc(-x,b,s) + sqfunc(B2-x,b,s)
+}
+
+
 #' Extract exponential growth rate assuming exponential growth
 #' 
 #' This function fits a linear model to ln(abundance) data.
@@ -192,6 +200,56 @@ get.gr.sat<-function(x,y,plotQ=F,fpath=NA,id=''){
   return(fit.sat)
 }
 
+#' Extract exponential growth rate assuming exponential death that hits a floor
+#' 
+#' This function fits a smoothed piecewise linear model to ln(abundance) data, with 
+#' the assumption that abundances decrease linearly at first, but then hit a floor
+#' and remain constant - consistent with a population that declines to the detection
+#' limit of fluoresence.
+#' 
+#' @param x Time steps
+#' @param y ln(abundance)
+#' @param plotQ logical; should the fit be plotted?
+#' @param fpath character; path specifying where plot should be saved, if generated
+#' @param id Label corresponding to the population/strain/species of interest
+#' 
+#' @return This function returns a nonlinear least-squares regression model
+#' 
+#' @examples 
+#' 
+#' @export
+#' @import minpack.lm
+#' @import zoo
+get.gr.flr<-function(x,y,plotQ=F,fpath=NA,id=''){
+  
+  data<-data.frame(x=x,y=y)
+  slopes <- rollapply(data.frame(x=x,y=y), 3, localslope, by.column=F)
+  a.guess<-coef(lm(y~x))[[1]]
+  
+  fit.flr<-nlsLM(y ~ flr(x,a,b,B2,s=1E-10),
+                 start=c(a=a.guess,b=min(c(-0.1,round(min(slopes),5))),B2=mean(x)),
+                 data = data,
+                 upper = c(a=Inf,b=0,B2=Inf),
+                 control = nls.control(maxiter=1000, warnOnly=TRUE))
+  cfs<-data.frame(t(coef(fit.flr)))
+  
+  if(plotQ){
+    if(!is.na(fpath)){
+      pdf(fpath)
+      plot(y~x,xlab='Time (days)',ylab='ln(fluorescence)',main=id)
+      curve(flr(x,cfs$a,cfs$b,cfs$B2,s=1E-10),cfs$B2,max(x),add=T,col='blue')
+      curve(flr(x,cfs$a,cfs$b,cfs$B2,s=1E-10),min(x),cfs$B2,add=T,col='red')
+      dev.off()
+    }else{
+      plot(y~x,xlab='Time (days)',ylab='ln(fluorescence)',main=id)
+      curve(flr(x,cfs$a,cfs$b,cfs$B2,s=1E-10),cfs$B2,max(x),add=T,col='blue')
+      curve(flr(x,cfs$a,cfs$b,cfs$B2,s=1E-10),min(x),cfs$B2,add=T,col='red')
+    }
+  }
+  return(fit.flr)
+}
+
+
 #' Extract exponential growth rate assuming lagged exponential growth that saturates
 #' 
 #' This function fits a smoothed piecewise linear model to ln(abundance) data, with 
@@ -284,7 +342,7 @@ detect<-function(x){
 #' @param y ln(abundance)
 #' @param plot.best.Q logical; should the best fitting model be plotted?
 #' @param fpath character; if best model is to be plotted, provide the file path for saving the plot
-#' @param methods Must be a character vector containing one or more of \code{'linear'}, \code{'lag'}, \code{'sat'}, or \code{'lagsat'}
+#' @param methods Must be a character vector containing one or more of \code{'linear'}, \code{'lag'}, \code{'sat'}, \code{'flr'}, or \code{'lagsat'}
 #' @param id Label corresponding to the population/strain/species of interest; used to determine the title and file name of saved plot, if any.
 #' @param model.selection control parameter to specify which IC metric to use in model selection; default is AICc, which corrects for small sample sizes and converges asymptotically on AIC.
 #' @param internal.r2.cutoff control parameter specifying the R2 criteria that may be applied to drop fits where the number of observations in the exponential portion is equal to 3. The default value of zero permits all fits of 3 obs to be considered.
@@ -296,26 +354,26 @@ detect<-function(x){
 #' 
 #' @export
 #' @import bbmle
-get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag','sat','lagsat'),model.selection=c('AICc'),internal.r2.cutoff=0,verbose=FALSE){
+get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag','sat','flr','lagsat'),model.selection=c('AICc'),internal.r2.cutoff=0,verbose=FALSE){
   
   # thin vectors if abundance measure is NA
   x<-x[!is.na(y)]
   y<-y[!is.na(y)]
   
-  if(sum(methods %in% c('linear','lag','sat','lagsat'))==0){
+  if(sum(methods %in% c('linear','lag','sat','flr','lagsat'))==0){
     print('Error! None of the specified methods matched a currently implemented approach')
   }
   
   if(verbose){print(paste('data set id = ',id))}
   
   # Initialize empty data structures
-  modlist<-list(gr=NA,gr.lag=NA,gr.sat=NA,gr.lagsat=NA)
-  class(modlist$gr)<-class(modlist$gr.lag)<-class(modlist$gr.sat)<-class(modlist$gr.lagsat)<-'try-error'
-  gr<-gr.lag<-gr.sat<-gr.lagsat<-NA
-  slope.gr<-slope.gr.lag<-slope.gr.sat<-slope.gr.lagsat<-NA
-  slope.n.gr<-slope.n.gr.lag<-slope.n.gr.sat<-slope.n.gr.lagsat<-NA
-  slope.r2.gr<-slope.r2.gr.lag<-slope.r2.gr.sat<-slope.r2.gr.lagsat<-NA
-  se.gr<-se.gr.lag<-se.gr.sat<-se.gr.lagsat<-NA
+  modlist<-list(gr=NA,gr.lag=NA,gr.sat=NA,gr.flr=NA,gr.lagsat=NA)
+  class(modlist$gr)<-class(modlist$gr.lag)<-class(modlist$gr.sat)<-class(modlist$gr.flr)<-class(modlist$gr.lagsat)<-'try-error'
+  gr<-gr.lag<-gr.sat<-gr.flr<-gr.lagsat<-NA
+  slope.gr<-slope.gr.lag<-slope.gr.sat<-slope.gr.flr<-slope.gr.lagsat<-NA
+  slope.n.gr<-slope.n.gr.lag<-slope.n.gr.sat<-slope.n.gr.flr<-slope.n.gr.lagsat<-NA
+  slope.r2.gr<-slope.r2.gr.lag<-slope.r2.gr.sat<-slope.r2.gr.flr<-slope.r2.gr.lagsat<-NA
+  se.gr<-se.gr.lag<-se.gr.sat<-se.gr.flr<-se.gr.lagsat<-NA
   
   if(length(unique(x))==2){
     print('Caution: only two unique time points, high risk of over-fitting. Methods other than "linear" are likely to fail')
@@ -385,6 +443,34 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
       }
       modlist$gr.sat<-gr.sat
     }
+    
+    if('flr' %in% methods){
+      gr.flr<-try(get.gr.flr(x,y))
+      if(prod(class(gr.flr)!='try-error')){
+        b2.cutoff <- coef(gr.flr)['B2']+0.1  # where does exponential phase end?
+        pds.flr <- predict(gr.flr)[x<=b2.cutoff] # predicted values below this cutoff
+        obs.flr <- y[x<=b2.cutoff] # observed values below this cutoff
+        
+        slope.gr.flr <- unname(coef(gr.flr)['b'])
+        slope.n.gr.flr <- length(x[x<=b2.cutoff])  # how many observations below cutoff
+        slope.r2.gr.flr <- 1-sum((pds.flr-obs.flr)^2)/sum((obs.flr-mean(obs.flr))^2)  # r2
+        se.gr.flr <- sqrt(diag(vcov(gr.flr)))['b']
+        
+        # if exponential portion is based on fewer than 3 observations, re-classify this fit as
+        # resulting in an error. This removes it from consideration as a 'best model', allowing
+        # a different model to succeed.
+        if(slope.n.gr.flr<3 | (slope.n.gr.flr==3 & slope.r2.gr.flr<internal.r2.cutoff)){
+          class(gr.flr)<-'try-error'          
+        }
+      }else{
+        slope.gr.flr <- NA
+        slope.n.gr.flr <- NA
+        slope.r2.gr.flr <- NA
+        se.gr.flr <- NA
+      }
+      modlist$gr.flr<-gr.flr
+    }
+    
     if('lagsat' %in% methods){
       gr.lagsat<-try(get.gr.lagsat(x,y))
       if(prod(class(gr.lagsat)!='try-error')){
@@ -424,12 +510,12 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
     }
   
     # assemble model names, contents, and slopes, but only for successful fits
-    mod.names<-c('gr','gr.lag','gr.sat','gr.lagsat')[successful.fits]
-    mod.list<-list(gr,gr.lag,gr.sat,gr.lagsat)[successful.fits]
-    slope.ests<-unname(c(slope.gr,slope.gr.lag,slope.gr.sat,slope.gr.lagsat)[successful.fits])
-    se.ests<-unname(c(se.gr,se.gr.lag,se.gr.sat,se.gr.lagsat)[successful.fits])
-    slope.n.vals<-c(slope.n.gr,slope.n.gr.lag,slope.n.gr.sat,slope.n.gr.lagsat)[successful.fits]
-    slope.r2.vals<-c(slope.r2.gr,slope.r2.gr.lag,slope.r2.gr.sat,slope.r2.gr.lagsat)[successful.fits]
+    mod.names<-c('gr','gr.lag','gr.sat','gr.flr','gr.lagsat')[successful.fits]
+    mod.list<-list(gr,gr.lag,gr.sat,gr.flr,gr.lagsat)[successful.fits]
+    slope.ests<-unname(c(slope.gr,slope.gr.lag,slope.gr.sat,slope.gr.flr,slope.gr.lagsat)[successful.fits])
+    se.ests<-unname(c(se.gr,se.gr.lag,se.gr.sat,se.gr.flr,se.gr.lagsat)[successful.fits])
+    slope.n.vals<-c(slope.n.gr,slope.n.gr.lag,slope.n.gr.sat,slope.n.gr.flr,slope.n.gr.lagsat)[successful.fits]
+    slope.r2.vals<-c(slope.r2.gr,slope.r2.gr.lag,slope.r2.gr.sat,slope.r2.gr.flr,slope.r2.gr.lagsat)[successful.fits]
       
     # compare successful models
     switch(model.selection,
@@ -454,7 +540,7 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
                  ses=se.ests,
                  slope.ns=slope.n.vals,
                  slope.rs=slope.r2.vals,
-                 models=list(gr=gr,gr.lag=gr.lag,gr.sat=gr.sat,gr.lagsat=gr.lagsat))
+                 models=list(gr=gr,gr.lag=gr.lag,gr.sat=gr.sat,gr.flr=gr.flr,gr.lagsat=gr.lagsat))
     #print(result)
   
     if(plot.best.Q){
@@ -468,6 +554,7 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
                  gr=get.gr(x,y,plotQ=T,fpath=fpath,id=id[1]),
                  gr.lag=get.gr.lag(x,y,plotQ=T,fpath=fpath,id=id[1]),
                  gr.sat=get.gr.sat(x,y,plotQ=T,fpath=fpath,id=id[1]),
+                 gr.flr=get.gr.flr(x,y,plotQ=T,fpath=fpath,id=id[1]),
                  gr.lagsat=get.gr.lagsat(x,y,plotQ=T,fpath=fpath,id=id[1]))
     }
   }else{
@@ -484,7 +571,7 @@ get.growth.rate<-function(x,y,id,plot.best.Q=F,fpath=NA,methods=c('linear','lag'
                  ses=NA,
                  slope.ns=NA,
                  slope.rs=NA,
-                 models=list(gr=NA,gr.lag=NA,gr.sat=NA,gr.lagsat=NA))
+                 models=list(gr=NA,gr.lag=NA,gr.sat=NA,gr.flr=NA,gr.lagsat=NA))
   }
   
   return(result)
