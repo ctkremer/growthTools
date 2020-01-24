09cf45c914b1137848d50065e8a30a8512b65720
ctkremer <colin.kremer@yale.edu>
2020-01-24 10:52:20 -0500

    adding general package level documentation

---
+++ R/growthTools.R
@@ -0,0 +1,12 @@
+#' growthTools: A package for computing microbial growth rates and traits.
+#'
+#' The growthTools package does X, Y, and Z. (see: http://r-pkgs.had.co.nz/man.html#man-packages)
+#' 
+#' @section get.gr functions:
+#' The get.gr family of functions does ...
+#'
+#' @docType package
+#' @name growthTools
+#' 
+#' @import methods
+NULL
