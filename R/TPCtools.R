4245c1700824d83d97d68fcc93b97ce7a03f7f62
ctkremer <colin.kremer@yale.edu>
2020-01-30 14:18:57 -0500

    plot options no longer accepted by get.nbcurve.tpc - these were moved to plot.tpc function instead.

--- R/TPCtools.R
+++ R/TPCtools.R
@@ -159,8 +159,7 @@ decurve2<-function(temperature,topt,phi,b2,d0,d2){
 #' @examples 
 #' data("example_TPC_data")
 #' sp1 <- example_TPC_data %>% filter(isolate.id=='CH30_4_RI_03' & dilution==1)
-#' nbcurve.traits<-get.nbcurve.tpc(sp1$temperature,sp1$mu,method='grid.mle2',
-#' plotQ=TRUE,conf.bandQ = TRUE,fpath=NA)
+#' nbcurve.traits<-get.nbcurve.tpc(sp1$temperature,sp1$mu,method='grid.mle2')
 #' nbcurve.traits
 #' 
 #' @export
