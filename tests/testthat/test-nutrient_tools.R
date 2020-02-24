9ff3f6b84181b541d57222fe91f6351bcfb8b86b
ctkremer <colin.kremer@yale.edu>
2020-02-24 13:48:01 -0500

    cleaned up unit tests for monod fitting

--- tests/testthat/test-nutrient_tools.R
+++ tests/testthat/test-nutrient_tools.R
@@ -30,7 +30,7 @@ test_that("Monod type 2 fitting in detail", {
 })
 
 
-test_that("Monod type 2 fitting in detail", {
+test_that("Monod type 2 fitting in detail, negative intercept", {
   monod.tmp<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52)-0.1)
   #plot(mu~nutrients,data=monod.tmp)
   
@@ -51,12 +51,14 @@ test_that("Monod type 2 fitting in detail", {
   fit0<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=FALSE),sd=exp(s)),
                     start=tmp.cfs,control=list(maxit=0),
                     data=monod.tmp)
-  bbmle::vcov(fit)
-  bbmle::vcov(fit0)
-  bbmle::summary(fit0)
-  ?bbmle::vcov
   
-  expect_equal(coef(fit)[[1]],-0.3890017,tolerance=1E-7)
+  expect_equal(coef(fit)[['umax']],-0.3890033 ,tolerance=1E-7)
+  expect_equal(coef(fit)[['k']],0.3570716 ,tolerance=1E-7)
+  expect_equal(coef(fit)[['z']],-1.926067 ,tolerance=1E-6)
+  expect_equal(coef(fit0)[['umax']],0.677732 ,tolerance=1E-7)
+  expect_equal(coef(fit0)[['k']],1.429138 ,tolerance=1E-7)
+  expect_equal(coef(fit0)[['z']],0.1457202 ,tolerance=1E-6)
+  
 })
 
 test_that("Monod fitting with get.monod, intercept fixed at 0", {
@@ -73,12 +75,12 @@ test_that("Monod fitting with get.monod, intercept fixed at 0", {
   attr(tmp.cis,"dimnames")<-list(c("umax","k","s"),c("2.5 %","97.5 %"))
   
   expect_equal(fit.monod$display_cf$umax,0.6504832,tolerance=1E-7)
-  expect_equal(fit.monod$display_cf$k,1.808149,tolerance=1E-7)
+  expect_equal(fit.monod$display_cf$k,1.808149,tolerance=1E-6)
   expect_equal(fit.monod$display_cf$s,-3.327181,tolerance=1E-7)
   
   expect_equal(fit.monod$rsqr,0.9658253,tolerance=1E-7)
   expect_equal(as.numeric(fit.monod$logLik),11.44946,tolerance=1E-5)
-  expect_equal(fit.monod$aic,-16.89892,tolerance=1E-7)
+  expect_equal(fit.monod$aic,-16.89892,tolerance=1E-6)
   expect_equal(cis,tmp.cis,tolerance=1E-7)
 })
 
@@ -90,18 +92,18 @@ test_that("Monod fitting with get.monod2, estimating z, true intercept ~=0", {
   cis<-fit.monod2$display_ci
   #plot(fit.monod2)
   
-  tmp.cis<-matrix(c(0.5510423,1.0216824,NaN,-3.8929874,
-                    0.7499246,2.5945958,NaN,-2.7613863),nrow=4,ncol=2)
+  tmp.cis<-matrix(c(0.58634888,0.69957896,-0.02728463,-4.01165800,
+                    0.7691173,2.1586835,0.1187264,-2.8800513),nrow=4,ncol=2)
   attr(tmp.cis,"dimnames")<-list(c("umax","k","z","s"),c("2.5 %","97.5 %"))
   
-  expect_equal(fit.monod2$umax,0.6504835,tolerance=1E-7)
-  expect_equal(fit.monod2$k,1.808139,tolerance=1E-7)
-  expect_equal(exp(fit.monod2$cf$z),1e-06,tolerance=1E-7)
-  expect_equal(fit.monod2$cf$s,-3.327187,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$umax,0.6777331,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$k,1.429131,tolerance=1E-6)
+  expect_equal(fit.monod2$display_cf$z,0.04572087,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$s,-3.445855,tolerance=1E-6)
   
-  expect_equal(fit.monod2$rsqr,0.9658256,tolerance=1E-7)
-  expect_equal(as.numeric(fit.monod2$logLik),11.44949,tolerance=1E-5)
-  expect_equal(fit.monod2$aic,-14.89898,tolerance=1E-7)
+  expect_equal(fit.monod2$rsqr,0.9730458,tolerance=1E-7)
+  expect_equal(as.numeric(fit.monod2$logLik),12.1615,tolerance=1E-5)
+  expect_equal(fit.monod2$aic,-16.323,tolerance=1E-6)
   expect_equal(cis,tmp.cis,tolerance=1E-7)
 })
 
@@ -114,17 +116,17 @@ test_that("Monod fitting with get.monod, estimating z, true intercept below 0", 
   #plot(fit.monod2)
   #fit.monod2
   
-  tmp.cis<-matrix(c(0.5510423,1.0216824,NaN,-3.8929874,
-                    0.7499246,2.5945958,NaN,-2.7613863),nrow=4,ncol=2)
+  tmp.cis<-matrix(c(0.58634777,0.69957347,0.07271423,-4.01165781,
+                    0.7691163,2.1587029,0.2187262,-2.8800506),nrow=4,ncol=2)
   attr(tmp.cis,"dimnames")<-list(c("umax","k","z","s"),c("2.5 %","97.5 %"))
   
-  expect_equal(fit.monod$umax,0.6504835,tolerance=1E-7)
-  expect_equal(fit.monod$k,1.808139,tolerance=1E-7)
-  expect_equal(exp(fit.monod$cf$z),1e-06,tolerance=1E-7)
-  expect_equal(fit.monod$cf$s,-3.327187,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$umax,0.677732,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$k,1.429138,tolerance=1E-6)
+  expect_equal(fit.monod2$display_cf$z,0.1457202,tolerance=1E-7)
+  expect_equal(fit.monod2$display_cf$s,-3.445854,tolerance=1E-7)
   
-  expect_equal(fit.monod$rsqr,0.9658256,tolerance=1E-7)
-  expect_equal(as.numeric(fit.monod$logLik),11.44949,tolerance=1E-5)
-  expect_equal(fit.monod$aic,-14.89898,tolerance=1E-7)
+  expect_equal(fit.monod2$rsqr,0.9730458,tolerance=1E-7)
+  expect_equal(as.numeric(fit.monod2$logLik),12.1615,tolerance=1E-5)
+  expect_equal(fit.monod2$aic,-16.323,tolerance=1E-6)
   expect_equal(cis,tmp.cis,tolerance=1E-7)
 })
