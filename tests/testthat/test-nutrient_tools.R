e5fbed4f8b677e604f39fd6d2d1b6564a35ea16a
ctkremer <colin.kremer@yale.edu>
2020-02-24 13:07:45 -0500

    adding expanded set of tests for monod type 2 functions

--- tests/testthat/test-nutrient_tools.R
+++ tests/testthat/test-nutrient_tools.R
@@ -3,39 +3,78 @@ library(growthTools)
 
 test_that("Monod fitting in detail", {
   monod.tmp<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52))
-  #plot(mu~nutrients,data=dat)
+  #plot(mu~nutrients,data=monod.tmp)
   
-  umax.guess <- max(monod.tmp$mu)[1]
   k.guess <- max(monod.tmp$nutrients)[1]/3
-  z.guess <- 0
+  umax.guess <- max(c(max(monod.tmp$mu)[1],0.01))
   
-  fit<-bbmle::mle2(mu~dnorm(mean=monod_curve(nutrients,umax,k,z),sd=exp(s)),
-                   start=list(umax=umax.guess,k=k.guess,z=z.guess,s=log(2)),
-                   fixed=list(z=0),data=monod.tmp)
+  fit<-bbmle::mle2(mu~dnorm(mean=monod_curve(nutrients,umax,k,log.k=TRUE,log.umax=TRUE),sd=exp(s)),
+                   start=list(umax=log(umax.guess),k=log(k.guess),s=log(2)),data=monod.tmp)
   
-  expect_equal(coef(fit)[[1]],0.650484,tolerance=1E-7)
+  expect_equal(coef(fit)[[1]],-0.4300399,tolerance=1E-7)
 })
 
-test_that("Monod fitting with get.monod, fixed intercept", {
+test_that("Monod type 2 fitting in detail", {
+  monod.tmp<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52))
+  #plot(mu~nutrients,data=monod.tmp)
+  
+  k.guess <- max(monod.tmp$nutrients)[1]/3
+  z.guess <- max(c(0.000001,abs(min(monod.tmp$mu)[1])))
+  umax.guess <- max(c(max(monod.tmp$mu)[1],0.01))+z.guess
+  
+  fit<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=TRUE),sd=exp(s)),
+                   start=list(umax=log(umax.guess),k=log(k.guess),z=log(z.guess),s=log(2)),
+                   data=monod.tmp)
+  
+  expect_equal(coef(fit)[[1]],-0.3890017,tolerance=1E-7)
+})
+
+
+test_that("Monod type 2 fitting in detail", {
+  monod.tmp<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52)-0.1)
+  #plot(mu~nutrients,data=monod.tmp)
+  
+  k.guess <- max(monod.tmp$nutrients)[1]/3
+  z.guess <- max(c(0.000001,abs(min(monod.tmp$mu)[1])))
+  umax.guess <- max(c(max(monod.tmp$mu)[1],0.01))+z.guess
+  
+  fit<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=TRUE),sd=exp(s)),
+                   start=list(umax=log(umax.guess),k=log(k.guess),z=log(z.guess),s=log(2)),
+                   data=monod.tmp)
+  
+  # reframe result on non-logged scale
+  model_cf<-as.list(coef(fit))
+  tmp.cfs<-model_cf
+  tmp.cfs$umax<-exp(tmp.cfs$umax)
+  tmp.cfs$k<-exp(tmp.cfs$k)
+  tmp.cfs$z<-exp(tmp.cfs$z)
+  fit0<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=FALSE),sd=exp(s)),
+                    start=tmp.cfs,control=list(maxit=0),
+                    data=monod.tmp)
+  bbmle::vcov(fit)
+  bbmle::vcov(fit0)
+  bbmle::summary(fit0)
+  ?bbmle::vcov
+  
+  expect_equal(coef(fit)[[1]],-0.3890017,tolerance=1E-7)
+})
+
+test_that("Monod fitting with get.monod, intercept fixed at 0", {
   dat<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52))
   #plot(mu~nutrients,data=dat)
   
-  fit.monod<-get.monod(nutrients=dat$nutrients,mu=dat$mu,method='mle2',fix_intercept = T)
-  cis<-fit.monod$cf_ciFI
-  #fit.monod$cf
+  fit.monod<-get.monod(nutrients=dat$nutrients,mu=dat$mu,method='mle2')
+  cis<-fit.monod$display_ci
+  #fit.monod$display_cf
   #plot(fit.monod)
   
-  tmp.cis<-matrix(c(0.5510411,1.0216724,-3.8929826,
-                    0.7499269,2.5946336,-2.7613688),nrow=3,ncol=2)
+  tmp.cis<-matrix(c(0.551041,1.021674,-3.892985,
+                    0.7499253,2.5946229,-2.7613773),nrow=3,ncol=2)
   attr(tmp.cis,"dimnames")<-list(c("umax","k","s"),c("2.5 %","97.5 %"))
   
-  #fit.monod.B<-get.monod(nutrients=dat$nutrients,mu=dat$mu,method='mle2',fix_intercept = F)
-  #plot(fit.monod.B)
-  
-  expect_equal(fit.monod$cf$umax,0.650484,tolerance=1E-7)
-  expect_equal(fit.monod$cf$k,1.808153,tolerance=1E-7)
-  expect_equal(fit.monod$cf$z,0,tolerance=1E-7)
-  expect_equal(fit.monod$cf$s,-3.327176,tolerance=1E-7)
+  expect_equal(fit.monod$display_cf$umax,0.6504832,tolerance=1E-7)
+  expect_equal(fit.monod$display_cf$k,1.808149,tolerance=1E-7)
+  expect_equal(fit.monod$display_cf$s,-3.327181,tolerance=1E-7)
   
   expect_equal(fit.monod$rsqr,0.9658253,tolerance=1E-7)
   expect_equal(as.numeric(fit.monod$logLik),11.44946,tolerance=1E-5)
@@ -43,25 +82,49 @@ test_that("Monod fitting with get.monod, fixed intercept", {
   expect_equal(cis,tmp.cis,tolerance=1E-7)
 })
 
-test_that("Monod fitting with get.monod, estimated intercept", {
+test_that("Monod fitting with get.monod2, estimating z, true intercept ~=0", {
   dat<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52))
   #plot(mu~nutrients,data=dat)
   
-  fit.monod<-get.monod(nutrients=dat$nutrients,mu=dat$mu,method='mle2',fix_intercept = F)
-  cis<-fit.monod$ciFI
-  #plot(fit.monod)
+  fit.monod2<-get.monod2(nutrients=dat$nutrients,mu=dat$mu,method='mle2')
+  cis<-fit.monod2$display_ci
+  #plot(fit.monod2)
   
-  tmp.cis<-matrix(c(0.5863497,0.6995717,-0.1187275,-4.0116577,
-                    0.76911820,2.15870318,0.02728491,-2.88005030),nrow=4,ncol=2)
+  tmp.cis<-matrix(c(0.5510423,1.0216824,NaN,-3.8929874,
+                    0.7499246,2.5945958,NaN,-2.7613863),nrow=4,ncol=2)
   attr(tmp.cis,"dimnames")<-list(c("umax","k","z","s"),c("2.5 %","97.5 %"))
   
-  expect_equal(fit.monod$cf$umax,0.6777339,tolerance=1E-7)
-  expect_equal(fit.monod$cf$k,1.429137,tolerance=1E-7)
-  expect_equal(fit.monod$cf$z,-0.04572128,tolerance=1E-7)
-  expect_equal(fit.monod$cf$s,-3.445854,tolerance=1E-7)
+  expect_equal(fit.monod2$umax,0.6504835,tolerance=1E-7)
+  expect_equal(fit.monod2$k,1.808139,tolerance=1E-7)
+  expect_equal(exp(fit.monod2$cf$z),1e-06,tolerance=1E-7)
+  expect_equal(fit.monod2$cf$s,-3.327187,tolerance=1E-7)
   
-  expect_equal(fit.monod$rsqr,0.9730458,tolerance=1E-7)
-  expect_equal(as.numeric(fit.monod$logLik),12.1615,tolerance=1E-5)
-  expect_equal(fit.monod$aic,-16.323,tolerance=1E-7)
+  expect_equal(fit.monod2$rsqr,0.9658256,tolerance=1E-7)
+  expect_equal(as.numeric(fit.monod2$logLik),11.44949,tolerance=1E-5)
+  expect_equal(fit.monod2$aic,-14.89898,tolerance=1E-7)
+  expect_equal(cis,tmp.cis,tolerance=1E-7)
+})
+
+test_that("Monod fitting with get.monod, estimating z, true intercept below 0", {
+  dat<-data.frame(nutrients=c(0.1,0.5,1,2,5,10),mu=c(0.02,0.1,0.2,0.4,0.5,0.52)-0.1)
+  #plot(mu~nutrients,data=dat)
+  
+  fit.monod2<-get.monod2(nutrients=dat$nutrients,mu=dat$mu,method='mle2')
+  cis<-fit.monod2$display_ci
+  #plot(fit.monod2)
+  #fit.monod2
+  
+  tmp.cis<-matrix(c(0.5510423,1.0216824,NaN,-3.8929874,
+                    0.7499246,2.5945958,NaN,-2.7613863),nrow=4,ncol=2)
+  attr(tmp.cis,"dimnames")<-list(c("umax","k","z","s"),c("2.5 %","97.5 %"))
+  
+  expect_equal(fit.monod$umax,0.6504835,tolerance=1E-7)
+  expect_equal(fit.monod$k,1.808139,tolerance=1E-7)
+  expect_equal(exp(fit.monod$cf$z),1e-06,tolerance=1E-7)
+  expect_equal(fit.monod$cf$s,-3.327187,tolerance=1E-7)
+  
+  expect_equal(fit.monod$rsqr,0.9658256,tolerance=1E-7)
+  expect_equal(as.numeric(fit.monod$logLik),11.44949,tolerance=1E-5)
+  expect_equal(fit.monod$aic,-14.89898,tolerance=1E-7)
   expect_equal(cis,tmp.cis,tolerance=1E-7)
 })
