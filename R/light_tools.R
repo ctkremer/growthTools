5854583579d08d7048337e77345f00aca02db97d
ctkremer <colin.kremer@yale.edu>
2021-10-08 21:25:31 -0700

    Set up functions and methods for fitting light performance curves, mimicking code for nutrient curves

--- R/light_tools.R
+++ R/light_tools.R
@@ -2,92 +2,76 @@
 
 #' Eiler-Peeters curve equation
 #' 
-#' Details here.
+#' See Eilers & Peeters 1988, Ecological Modeling. Function allows for linearly increasing growth rates at low light levels, 
+#' which saturate at maximum growth rate of umax at the optimum light level Lopt, and then decline as light levels increase
+#' due to photoinhibition. 
 #' 
-#' @param x nutrients
+#' @param x light level, typically umol photons m^-2 s^-1
+#' @param a slope of initial increase in growth rate at low light levels, aka affinity
 #' @param umax maximum growth rate
-#' @param k half saturation constant
-#' @param log.k logical, is k specified on natural log scale
+#' @param Lopt optimal light level where maximum growth occurs
+#' @param log.a logical, is a specified on natural log scale
 #' @param log.umax logical, is umax specified on natural log scale
+#' @param log.Lopt logical, is Lopt specified on natural log scale
 #' 
-#' @return Predicted exponential growth rate at nutrient concentration x
+#' @return Predicted exponential growth rate at light level x
 #' 
 #' @export
-ep_curve<-function(x,umax,k,log.k=FALSE,log.umax=FALSE){
-  if(log.k){
-    res<-x/(x+exp(k))
-  }else{
-    res<-x/(x+k)
+ep_curve<-function(x,a,umax,Lopt,log.a=FALSE,log.umax=FALSE,log.Lopt=FALSE){
+  if(log.a){
+    a<-exp(a)
   }
   if(log.umax){
-    res<-exp(umax)*res
-  }else{
-    res<-umax*res
+    umax<-exp(umax)
   }
-  res
-}
-
-#' Monod curve with loss term equation
-#' 
-#' Details here.
-#' 
-#' @param x nutrients
-#' @param umax maximum growth rate
-#' @param k half saturation constant
-#' @param z ln(intercept)
-#' @param log.pars logical, are parameters specified on natural log scale
-#' 
-#' @return Predicted exponential growth rate at nutrient concentration x
-#' 
-#' @export
-monod_curve_type2<-function(x,umax,k,z,log.pars=FALSE){
-  if(log.pars){
-    res<-exp(umax)*x/(x+exp(k))-exp(z)
-  }else{
-    res<-umax*x/(x+k)-z
+  if(log.Lopt){
+    Lopt<-exp(Lopt)
   }
+  
+  res <- umax*(x/((umax*x^2)/(a*Lopt^2) + (1 - 2*(umaxL/(a*Lopt)))*x + umax/a))
+  
   res
 }
 
 
-#' Nutrient performance curve (NPC) class creation
+#' Light performance curve (LPC) class creation
 #' 
 #' @export
-new_npc<-function(){
-  npcObj<-list()
-  class(npcObj)<-"npc"
+new_lpc<-function(){
+  lpcObj<-list()
+  class(lpcObj)<-"lpc"
   
   # npc type
-  npcObj$type<-character()
+  lpcObj$type<-character()
   
   # estimated parameters from fit
-  npcObj$model_cf<-list()
-  npcObj$display_cf<-list()
-  npcObj$display_ci<-matrix()
-  npcObj$vcov<-matrix()
+  lpcObj$model_cf<-list()
+  lpcObj$display_cf<-list()
+  lpcObj$display_ci<-matrix()
+  lpcObj$vcov<-matrix()
   
   # fit diagnostics
-  npcObj$rsqr<-double()
-  npcObj$nnutr<-integer()
-  npcObj$nobs<-integer()
-  npcObj$logLik<-double()
-  npcObj$aic<-double()
+  lpcObj$rsqr<-double()
+  lpcObj$nlight<-integer()
+  lpcObj$nobs<-integer()
+  lpcObj$logLik<-double()
+  lpcObj$aic<-double()
   
   # data
-  npcObj$data<-data.frame()
+  lpcObj$data<-data.frame()
   
-  npcObj
+  lpcObj
 }
 
 
-#' Print method for NPC objects
+#' Print method for LPC objects
 #' 
-#' @param x Object of class npc
+#' @param x Object of class lpc
 #' @param \dots Additional arguments (not used)
 #' 
 #' @export
-print.npc<-function(x,...){
-  cat("Nutrient performance Curve fit\n")
+print.lpc<-function(x,...){
+  cat("Light performance Curve fit\n")
   cat("Model type: ",x$type,"\n")
   cat("\nEstimated parameters:\n")
   tmp<-cbind(x$display_cf,x$display_ci)
@@ -104,16 +88,16 @@ print.npc<-function(x,...){
 }
 
 
-#' Predict values from an NPC fit
+#' Predict values from an LPC fit
 #' 
-#' @param object The result of a single NPC curve fit from get.monod, an object of class npc
-#' @param newdata A new data frame, containing a sequence of `nutrient` values at which model predictions should be made; defaults to (0,100)
+#' @param object The result of a single LPC curve fit, an object of class lpc
+#' @param newdata A new data frame, containing a sequence of `light` values at which model predictions should be made; defaults to (0,400)
 #' @param se.fit logical; should standard error values be returned?
 #' @param \dots Additional arguments (not used)
 #' 
 #' @export
-#' @method predict npc
-predict.npc<-function(object,newdata,se.fit=FALSE,...){
+#' @method predict lpc
+predict.lpc<-function(object,newdata,se.fit=FALSE,...){
   
   # Check level of nesting for fit.info, and reduce if necessary
   if(length(object)==1){
@@ -129,20 +113,17 @@ predict.npc<-function(object,newdata,se.fit=FALSE,...){
   
   # generate predictions across a range of temperatures
   switch(object$type,
-         monod={mu<-monod_curve(newdata$nutrients,umax=object$model_cf$umax,k=object$model_cf$k,
-                                log.k=TRUE,log.umax=TRUE)},
-         monod2={mu<-monod_curve_type2(newdata$nutrients,umax=object$model_cf$umax,k=object$model_cf$k,
-                                       z=object$model_cf$z,log.pars=TRUE)},
-         stop(print("unrecognized npc model type in predict.npc!")))
+         ep={mu<-ep_curve(newdata$light, a=object$model_cf$a, umax=object$model_cf$umax, Lopt=object$model_cf$Lopt,
+                                log.a=TRUE, log.umax=TRUE, log.Lopt = TRUE)},
+         stop(print("unrecognized lpc model type in predict.lpc!")))
   newdata$mu<-mu
   
   if(se.fit){
-    insert<-paste(newdata$nutrients,collapse=',')
+    insert<-paste(newdata$light,collapse=',')
     
     switch(object$type,
-           monod={st<-paste("monod_curve(c(",insert,"),umax,k,log.k=TRUE,log.umax=TRUE)",sep='')},
-           monod2={st<-paste("monod_curve_type2(c(",insert,"),umax,k,z,log.pars=TRUE)",sep='')},
-           stop(print("unrecognized npc model type in predict.npc se calcs!")))
+           ep={st<-paste("ep_curve(c(",insert,"),a,umax,Lopt,log.a=TRUE,log.umax=TRUE,log.Lopt=TRUE)",sep='')},
+           stop(print("unrecognized lpc model type in predict.lpc se calcs!")))
     dvs0<-suppressWarnings(deltavar2(fun=parse(text=st),meanval=object$model_cf,Sigma=object$vcov))
     newdata$se.fit<-sqrt(dvs0)
   }
@@ -151,9 +132,9 @@ predict.npc<-function(object,newdata,se.fit=FALSE,...){
 }
 
 
-#' Plotting function for single nutrient performance curve
+#' Plotting function for single light performance curve
 #' 
-#' @param x The result of a single NPC curve fit from get.monod or similar
+#' @param x The result of a single LPC curve fit from get.ep or similar
 #' @param plot_ci logical, should the resulting plot include 95\% confidence bands
 #' @param plot_obs logical, should resulting plot include raw data
 #' @param xlim x-axis range (nutrients)
@@ -164,7 +145,7 @@ predict.npc<-function(object,newdata,se.fit=FALSE,...){
 #' 
 #' @export
 #' @import ggplot2
-plot.npc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpath=NA,...){
+plot.lpc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpath=NA,...){
   
   # Check level of nesting for fit.info, and reduce if necessary
   if(length(x)==1){
@@ -173,16 +154,16 @@ plot.npc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpat
   
   # adjust plotting window to specific curve?
   if(is.null(ylim)) ylim <- c(1.3*min(c(min(x$data$mu,na.rm=T)[1],0)),1.3*max(x$data$mu,na.rm=T)[1])
-  if(is.null(xlim)) xlim <- c(0,1.2*max(x$data$nutrients,na.rm=T)[1])
+  if(is.null(xlim)) xlim <- c(0,1.2*max(x$data$light,na.rm=T)[1])
   
   # generate predictions along a range of nutrients
-  preds<-predict(x,newdata=data.frame(nutrients=seq(xlim[1],xlim[2],length.out = 200)),se.fit = plot_ci)    
+  preds<-predict(x,newdata=data.frame(light=seq(xlim[1],xlim[2],length.out = 200)),se.fit = plot_ci)    
   
   # generate basic plot
-  cplot<-ggplot(preds,aes(x=.data$nutrients,y=.data$mu))+
+  cplot<-ggplot(preds,aes(x=.data$light,y=.data$mu))+
     geom_hline(yintercept = 0)+
     geom_line()+
-    scale_x_continuous('Nutrients')+
+    scale_x_continuous('Light')+
     scale_y_continuous('Growth rate')+
     coord_cartesian(xlim=xlim,ylim=ylim)+
     theme_bw()+
@@ -203,9 +184,9 @@ plot.npc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpat
     if(is.na(main)){ # and a plot ID
       time<-Sys.time()
       time<-gsub(x = time,pattern = ":",replacement = "_")
-      full.path<-paste(fpath,"NPC_fit_",time,".pdf",sep='')        
+      full.path<-paste(fpath,"LPC_fit_",time,".pdf",sep='')        
     }else{ # or not
-      full.path<-paste(fpath,"NPC_fit_",main,".pdf",sep='') 
+      full.path<-paste(fpath,"LPC_fit_",main,".pdf",sep='') 
     }
     
     # save plot at full.path with given file name.
@@ -218,12 +199,12 @@ plot.npc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpat
 }
 
 
-#' Fit Monod curve to growth rate vs. nutrient concentration data
+#' Fit Eiler Peeters curve to growth rate vs. light data
 #' 
 #' Note: this function currently does not use grid.mle2, as the regressions are usually pretty stable; add this 
 #' feature later.
 #' 
-#' @param nutrients Nutrient concentration
+#' @param light Light level
 #' @param mu Exponential growth rate
 #' @param method Specify which fitting algorithm to use, 'mle2' or 'grid.mle2'
 #' @param ... Additional arguments passed to grid.mle2 (e.g., control=list(maxit=2000))
@@ -233,32 +214,34 @@ plot.npc<-function(x,plot_ci=TRUE,plot_obs=TRUE,xlim=NULL,ylim=NULL,main=NA,fpat
 #' @export
 #' @import emdbook
 #' @import ggplot2
-get.monod<-function(nutrients,mu,method='mle2',...){
-  monod.tmp<-stats::na.omit(data.frame(nutrients,mu))
-  nnutr<-length(unique(monod.tmp$nutrients))
+get.ep<-function(light,mu,method='mle2',...){
+  ep.tmp<-stats::na.omit(data.frame(light,mu))
+  nlight<-length(unique(ep.tmp$light))
   
-  if(nnutr<=3){
-    print("Caution in get.monod - focal data set has <=3 unique nutrients, risk of overfitting is high!")
+  if(nlight<=3){
+    print("Caution in get.ep - focal data set has <=3 unique light levels, risk of overfitting is high!")
   }
   
   if(method=='grid.mle2'){
-    print("Error: this option not yet implemented... 2/13/20")
+    print("Error: this option not yet implemented in get.ep ... 10/7/21")
   }
   
   if(method=='mle2'){
-    k.guess <- max(monod.tmp$nutrients)[1]/3
-    umax.guess <- max(c(max(monod.tmp$mu)[1],0.01))
-    fit<-bbmle::mle2(mu~dnorm(mean=monod_curve(nutrients,umax,k,log.k=TRUE,log.umax=TRUE),sd=exp(s)),
-                     start=list(umax=log(umax.guess),k=log(k.guess),s=log(2)),data=monod.tmp)
+    #a.guess <- max(monod.tmp$nutrients)[1]/3
+    umax.guess <- max(c(max(ep.tmp$mu)[1],0.01))
+    Lopt.guess <- max(c(ep.tmp$light[ep.tmp$mu==max(ep.tmp$mu)],10))
+    fit<-bbmle::mle2(mu~dnorm(mean=ep_curve(light,a,umax,Lopt,log.a=TRUE,log.umax=TRUE,log.Lopt = TRUE),sd=exp(s)),
+                     start=list(a=log(a.guess),umax=log(umax.guess),Lopt=log(Lopt.guess),s=log(2)),data=ep.tmp)
     
     # reframe result on non-logged scale
     model_cf<-as.list(coef(fit))
     tmp.cfs<-model_cf
+    tmp.cfs$a<-exp(tmp.cfs$a)
     tmp.cfs$umax<-exp(tmp.cfs$umax)
-    tmp.cfs$k<-exp(tmp.cfs$k)
-    fit0<-bbmle::mle2(mu~dnorm(mean=monod_curve(nutrients,umax,k,log.k=FALSE,log.umax=FALSE),sd=exp(s)),
+    tmp.cfs$Lopt<-exp(tmp.cfs$Lopt)
+    fit0<-bbmle::mle2(mu~dnorm(mean=ep_curve(light,a,umax,Lopt,log.k=FALSE,log.umax=FALSE),sd=exp(s)),
                       start=tmp.cfs,control=list(maxit=0),
-                      data=monod.tmp)
+                      data=ep.tmp)
   }
   
   # pull out parameters on regular scale
@@ -271,103 +254,23 @@ get.monod<-function(nutrients,mu,method='mle2',...){
   vcov_mat<-vcov(fit)
   
   # calculate R2
-  rsqr<-get.R2(predict(fit),monod.tmp$mu)
+  rsqr<-get.R2(predict(fit),ep.tmp$mu)
   
   # create empty object of class 'npc'
-  vec<-new_npc()
+  vec<-new_lpc()
   
   # populate npc object
-  vec$type<-'monod'
+  vec$type<-'ep'
   vec$model_cf<-model_cf
   vec$display_cf<-display_cf
   vec$display_ci<-ci
   vec$vcov<-vcov_mat
-  vec$nobs<-nrow(monod.tmp)
-  vec$nnutr<-nnutr
+  vec$nobs<-nrow(ep.tmp)
+  vec$nlight<-nlight
   vec$rsqr<-rsqr
   vec$logLik<-logLik(fit)
   vec$aic<-stats::AIC(fit)
-  vec$data<-monod.tmp
-  
-  # Finished, return relevant stats.  
-  return(vec)
-}
-
-
-#' Fit Monod curve with loss term to growth rate vs. nutrient concentration data
-#' 
-#' Note: this function currently does not use grid.mle2, as the regressions are usually pretty stable; add this 
-#' feature later.
-#' 
-#' @param nutrients Nutrient concentration
-#' @param mu Exponential growth rate
-#' @param method Specify which fitting algorithm to use, 'mle2' or 'grid.mle2'
-#' @param ... Additional arguments passed to grid.mle2 (e.g., control=list(maxit=2000))
-#' 
-#' @examples 
-#' 
-#' @export
-#' @import emdbook
-#' @import ggplot2
-get.monod2<-function(nutrients,mu,method='mle2',...){
-  monod.tmp<-stats::na.omit(data.frame(nutrients,mu))
-  nnutr<-length(unique(monod.tmp$nutrients))
-  
-  if(nnutr<=3){
-    print("Caution in get.monod - focal data set has <=3 unique nutrients, risk of overfitting is high!")
-  }
-  
-  if(method=='grid.mle2'){
-    print("Error: this option not yet implemented... 2/13/20")
-  }
-  
-  if(method=='mle2'){
-    
-    k.guess <- max(monod.tmp$nutrients)[1]/3
-    z.guess <- max(c(0.000001,abs(min(monod.tmp$mu)[1])))
-    umax.guess <- max(c(max(monod.tmp$mu)[1],0.01))+z.guess
-    fit<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=TRUE),sd=exp(s)),
-                     start=list(umax=log(umax.guess),k=log(k.guess),z=log(z.guess),s=log(2)),
-                     data=monod.tmp)
-    
-    # reframe result on non-logged scale
-    model_cf<-as.list(coef(fit))
-    tmp.cfs<-model_cf
-    tmp.cfs$umax<-exp(tmp.cfs$umax)
-    tmp.cfs$k<-exp(tmp.cfs$k)
-    tmp.cfs$z<-exp(tmp.cfs$z)
-    fit0<-bbmle::mle2(mu~dnorm(mean=monod_curve_type2(nutrients,umax,k,z,log.pars=FALSE),sd=exp(s)),
-                      start=tmp.cfs,control=list(maxit=0),
-                      data=monod.tmp)
-  }
-  
-  # pull out parameters on regular scale
-  display_cf<-as.list(coef(fit0))
-  
-  # simple Fisher confidence intervals on regular scale:
-  ci<-mleTools::ci.FI(fit0)
-  
-  # vcov on log scale
-  vcov_mat<-vcov(fit)
-  
-  # calculate R2
-  rsqr<-get.R2(predict(fit),monod.tmp$mu)
-  
-  # create empty object of class 'npc'
-  vec<-new_npc()
-  
-  # populate npc object
-  vec$type<-'monod2'
-  vec$model_cf<-model_cf
-  vec$display_cf<-display_cf
-  vec$display_ci<-ci
-  vec$vcov<-vcov_mat
-  vec$nobs<-nrow(monod.tmp)
-  vec$nnutr<-nnutr
-  vec$rsqr<-rsqr
-  vec$logLik<-logLik(fit)
-  vec$aic<-stats::AIC(fit)
-  vec$data<-monod.tmp
+  vec$data<-ep.tmp
   
   # Finished, return relevant stats.  
   return(vec)
