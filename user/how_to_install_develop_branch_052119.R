123704f843d64cb49b2399a7e6aab1efb923d348
ctkremer <colin.kremer@yale.edu>
2021-10-21 09:43:55 -0700

    updated installation instructions

--- user/how_to_install_develop_branch_052119.R
+++ user/how_to_install_develop_branch_052119.R
@@ -3,23 +3,24 @@
 # Need to install devtools, if not already there
 library(devtools)
 
-#issues to build vignettes, need a development version of pillar package
-devtools::install_github("r-lib/pillar")
+#### growthTools depends on mleTools, which you can install via:
+
+devtools::install_github("ctkremer/mleTools",upgrade='ask')
+
 
 #### Install development version of growthTools:
 
 # remove any old versions of the package before installing
 # remove.packages('growthTools')
 
-devtools::install_github("ctkremer/growthTools@develop",auth_token = 'f8fe6d72858a84490060478a38802cc873b172e9',build_vignettes = T,upgrade_dependencies=F,build_opts = c("--no-resave-data", "--no-manual"))
+# Install without building vignette:
+devtools::install_github("ctkremer/growthTools@develop",build_vignettes = F,upgrade='ask')
 
+# Note: if you want to build the vignette, change build_vignettes to T, may take ~10 min to build. Alternatively, view pre-built version.
+
+# Load growthTools
 library(growthTools)
 
+# IF you built the vignette, view it via:
 vignette("growthTools_vignette",package="growthTools")
-
-
-
-
-devtools::install_github("ctkremer/growthTools@master",auth_token = 'f8fe6d72858a84490060478a38802cc873b172e9',build_vignettes = T,upgrade_dependencies=F,build_opts = c("--no-resave-data", "--no-manual"))
-
 
